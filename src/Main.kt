0d7808d5f2d33ff8f145e87c1550a605464e2176
Catalin Darjan <catadarjan@gmail.com>
2020-04-19 17:33:23 +0300

    Add execution time calculation

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,19 @@
 import util.SolutionUtils
+import java.time.Instant
 
 fun main() {
     (1..10).forEach {
-        println("Test $it")
         val solutionUtils = SolutionUtils(it.toString())
         val graphData = solutionUtils.readGraphData()
         val dijkstra = Dijkstra(graphData.nodesCount, graphData.graph)
+        val startTime = Instant.now()
         val mySolution = dijkstra.determineShortestPathsSequential()
-        println(
+        val finishTime = Instant.now()
+        val isSolutionValid =
             solutionUtils.checkSolution(
                 mySolution
             )
-        )
+        println("Test $it passed: $isSolutionValid \n Algorithm ran in: ${finishTime.toEpochMilli() - startTime.toEpochMilli()}ms")
     }
 }
 
